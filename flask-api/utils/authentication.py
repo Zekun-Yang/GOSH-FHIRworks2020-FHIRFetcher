2892a14f4d75a6abac2f430c6671b5035fad2d6c
Zekun-Yang <zekun.yang@outlook.com>
2020-03-22 00:28:50 +0000

    remove credentials

--- flask-api/utils/authentication.py
+++ flask-api/utils/authentication.py
@@ -1,11 +1,11 @@
 import requests
-CLIENT_ID = "0f6332f4-c060-49fc-bcf6-548982d56569"
-CLIENT_SECRET = "ux@CJAaxCD85A9psm-Wdb?x3/Z4c6gp9"
-SCOPE = "https://gosh-fhir-synth.azurehealthcareapis.com/.default"
-FHIR_BASE_URL = "https://gosh-fhir-synth.azurehealthcareapis.com"
-payload = "grant_type=client_credentials&client_id={}&client_secret={}&scope={}".format(CLIENT_ID, CLIENT_SECRET, SCOPE)
-url = "https://login.microsoftonline.com/ca254449-06ec-4e1d-a3c9-f8b84e2afe3f/oauth2/v2.0/token"
-headers = { 'content-type': "application/x-www-form-urlencoded" }
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+SCOPE = ""
+FHIR_BASE_URL = ""
+payload = ""
+url = ""
+headers = { 'content-type': "" }
 def get_access_token():  
     res = requests.post(url, payload, headers=headers)
     if res.status_code == 200:
